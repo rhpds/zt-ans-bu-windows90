76d8050174f826b5e0daa9753cfa806aa91049aa
amoyament <amoyamen44@gmail.com>
2025-08-27 14:02:45 -0400

    Added vs code set up

--- setup-automation/setup-windows.sh
+++
@@ -1,4 +0,0 @@
-
-#. { iwr -useb https://raw.githubusercontent.com/nmartins0611/windows_getting_started_instruqt/main/winrm_setup.ps1 -OutFile .\winrm_setup.ps1 } 
-
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/nmartins0611/windows_getting_started_instruqt/main/winrm_setup.ps1 -OutFile .\winrm_setup.ps1; .\winrm_setup.ps1
