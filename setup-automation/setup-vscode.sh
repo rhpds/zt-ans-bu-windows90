ddecae8b136b8f85269a19fc936f52ac9e97da0f
Nuno Martins <nmartins@redhat.com>
2025-08-27 15:30:40 +0200

    Update and rename setup-node01.sh to setup-vscode.sh

---
+++ setup-automation/setup-vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+update-ca-trust
+rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
+
+subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
+
